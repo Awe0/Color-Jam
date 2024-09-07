13f0e8b90f7229413f5521e3e865df9a6a49fffb
Allan <allan.delatuillerie@gmail.com>
2024-09-07 15:23:18 +0200

    remove useless code

--- Scripts/color.gd
+++ Scripts/color.gd
@@ -3,9 +3,5 @@ extends Node2D
 @export var size = 0
 @export var score = 0
 @export var color_name = ""
-@onready var texture_rect: TextureRect = $TextureRect
 var is_vertical = true
 var start_position = Vector2(-1, -1)
-
-func _ready() -> void:
-	texture_rect.texture = load("res://Assets/"+color_name+"_preview.png")
