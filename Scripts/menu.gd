e3f429084a69451e7591d873a267e827d4656af3
Allan <allan.delatuillerie@gmail.com>
2024-09-09 11:24:09 +0200

    modif menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -8,3 +8,6 @@ func _on_play_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_login_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Login.tscn")
