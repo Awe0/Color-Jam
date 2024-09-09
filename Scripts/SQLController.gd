9be9f56c52f1adc712f2210dcd7a6fd82a53dc87
Allan <allan.delatuillerie@gmail.com>
2024-09-09 15:29:21 +0200

    add SQLController

---
+++ Scripts/SQLController.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var database: SQLite
+
+func _ready() -> void:
+	database = SQLite.new()
+	database.path="res://data.db"
+	database.open_db()
+	var table = {
+		"id" : {"data_type": "int", "primary_key": true, "not_null": true, "auto_increment": true},
+		"name": {"data_type": "text"},
+		"password": {"data_type": "text"},
+	}
+	database.create_table("userInfo",table)
