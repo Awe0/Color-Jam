6f5bae96c096ae01d15861f63c5d2fb12a37d7d1
Allan <allan.delatuillerie@gmail.com>
2024-09-09 15:28:52 +0200

    login system in progress

--- Scripts/login.gd
+++ Scripts/login.gd
@@ -1,11 +1,21 @@
 extends Control
 
+signal create_user(username: String, password: String)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var connection_username: TextEdit = $VBoxContainer/UsernameContainer/Label/MarginContainer/TextEdit
+@onready var connection_password : LineEdit = $VBoxContainer/PasswordContainer/Label/MarginContainer/LineEdit
 
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_signin_button_pressed() -> void:
+	create_user.emit(connection_username.text,connection_password.text)
+
+func _on_submit_button_pressed() -> void:
+	var data = {
+		"name": connection_username.text,
+		"password": connection_password.text,
+	}
+	SqlController.database.insert_row("userInfo",data)
+	connection_username.text = ""
+	connection_password.text = ""
