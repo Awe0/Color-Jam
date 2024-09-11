75f52a41933e33f6c86343e698fdd4f5b566ca22
Allan <allan.delatuillerie@gmail.com>
2024-09-11 15:42:19 +0200

    score in db ok

--- Scripts/UI/login.gd
+++ Scripts/UI/login.gd
@@ -7,19 +7,23 @@ signal create_user(username: String, password: String)
 @onready var message_timer: Timer = $Messages/Timer
 @onready var failed: Label = $Messages/Failed
 @onready var succes: Label = $Messages/Succes
+@onready var no_account: Label = $"Messages/No account"
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
 
 func _on_signin_button_pressed() -> void:
-	if connection_username.text and connection_password.text:
-		var query = SqlController.get_user_from_db(connection_username.text)
-		if connection_password.text == query["password"]:
-			UserSession.log_in_log_out()
-			UserSession.save_session(query["id"], query["name"])
-			get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
-		else:
-			print("NEIN NEIN NEIN")
+	var query = SqlController.get_user_from_db(connection_username.text)
+	if query != null:
+		if connection_username.text and connection_password.text:
+			if connection_password.text == query["password"]:
+				UserSession.log_in_log_out()
+				UserSession.save_session(query["id"], query["name"])
+				get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+			else:
+				print("NEIN")
+	else:
+		display_label_and_start_timer(no_account)
 
 func _on_submit_button_pressed() -> void:
 	if connection_username.text and connection_password.text:
@@ -27,22 +31,20 @@ func _on_submit_button_pressed() -> void:
 			"name": connection_username.text,
 			"password": connection_password.text,
 		}
-		SqlController.database.insert_row("userInfo",data)
+		SqlController.database.insert_row("users",data)
 		connection_username.text = ""
 		connection_password.text = ""
 		if failed.visible == true:
 			failed.visible == false
 		create_user.emit(connection_username.text,connection_password.text)
-		succes.visible = true
-		message_timer.start(5)
-		message_timer.timeout.connect(func() -> void:_on_timer_timeout())
+		display_label_and_start_timer(succes)
 	else:
-		failed.visible = true
-		message_timer.start(3)
-		message_timer.timeout.connect(func() -> void:_on_timer_timeout())
+		display_label_and_start_timer(failed)
 
+func _on_timer_timeout(label: Label) -> void:
+	label.visible = false
 
-
-func _on_timer_timeout() -> void:
-	failed.visible = false
-	succes.visible = false
+func display_label_and_start_timer(label: Label):
+	label.visible = true
+	message_timer.start(5)
+	message_timer.timeout.connect(func() -> void:_on_timer_timeout(label))
---
+++ Scripts/UI/scoreboard.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var scoreboard: Control = $"."
+
+func _ready():
+	pass
+
+func add_score_on_board():
+	pass
