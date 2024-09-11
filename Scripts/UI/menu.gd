00591d283acbe6a45152a39eaba91e2d6e424ee9
Allan <allan.delatuillerie@gmail.com>
2024-09-11 16:51:26 +0200

    Remove popup and scoreboard in progress

--- Scripts/UI/menu.gd
+++ Scripts/UI/menu.gd
@@ -19,7 +19,7 @@ func _on_login_button_pressed() -> void:
 
 func _on_scoreboard_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Scoreboard.tscn")
-	
+
 func display_buttons():
 	if UserSession.is_login == true:
 		play.visible = true
--- Scripts/UI/scoreboard.gd
+++ Scripts/UI/scoreboard.gd
@@ -1,9 +1,15 @@
 extends Control
 
-@onready var h_box_container: HBoxContainer = $VBoxContainer/HBoxContainer
+@onready var score_container: HBoxContainer = $VBoxContainer/HBoxContainer
 
 func _ready():
-	pass
-
+	add_score_on_board()
+	SignalBus.Game_is_over.connect(add_score_on_board)
+	
 func add_score_on_board():
-	pass
+	print("Signal 'Game_is_over' ok !")
+	var query = SqlController.get_scores_from_db(UserSession.session_config.get_value("user","user_id"))
+	var label = Label.new()
+	label.text = str(query)
+	score_container.add_child(label)
+	#score_container.add_child()
