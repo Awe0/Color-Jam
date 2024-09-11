6ac881ddb28d253d459c4b08bc3b69b88c61ad0f
Allan <allan.delatuillerie@gmail.com>
2024-09-11 18:35:15 +0200

    Copy database to user

--- Scripts/Singletons/SQLController.gd
+++ Scripts/Singletons/SQLController.gd
@@ -14,15 +14,42 @@ var scores_table = {
 }
 
 func _ready() -> void:
-	database = SQLite.new()
-	database.path = "res://data.db"
-	database.foreign_keys = true
-	database.open_db()
-	var db_in_user = database.duplicate()
-	print(database)
-	print(db_in_user)
+	copy_database_to_user()
+	open_database()
+
+func create_table():
 	database.create_table("users",users_table)
 	database.create_table("scores",scores_table)
+
+func open_database():
+	database = SQLite.new()
+	database.path = "user://data.db"
+	database.foreign_keys = true
+	var db_result = database.open_db()
+	if db_result:
+		print("Database opened successfully")
+	else:
+		print("Failed to open the database")
+
+func copy_database_to_user():
+	var db_path = "res://data.db"
+	var new_db_path = "user://data.db"
+	if FileAccess.file_exists(new_db_path):
+		print("Database already exists in user://")
+		return 
+	var file = FileAccess.open(db_path, FileAccess.ModeFlags.READ)
+	if file:
+		var data = file.get_buffer(file.get_length())
+		file.close()
+		var new_file = FileAccess.open(new_db_path, FileAccess.ModeFlags.WRITE)
+		if new_file:
+			new_file.store_buffer(data)
+			new_file.close()
+			print("Database copied successfully to user://")
+		else:
+			print("Error: Cannot write to user database path")
+	else:
+		print("Error: Cannot read from project database path")
 
 func get_user_from_db(username: String):
 	var query = database.query("SELECT * FROM users WHERE name = '"+username+"'")
