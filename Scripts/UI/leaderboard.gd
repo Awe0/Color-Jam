a32ffb29af03bbc3214321f29659b39e90c52791
Allan <allan.delatuillerie@gmail.com>
2024-09-21 10:38:47 +0200

    rm Login/Scoreboard scene and add Help

--- Scripts/UI/leaderboard.gd
+++ Scripts/UI/leaderboard.gd
@@ -2,7 +2,3 @@ extends Control
 
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
-
-
-func _on_local_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Scoreboard.tscn")
