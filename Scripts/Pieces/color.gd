0341287a9519a7047132bc89a34e95c3b33ebcd0
Allan <allan.delatuillerie@gmail.com>
2024-09-22 13:01:51 +0200

    add APKs on github

--- Scripts/Pieces/color.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@export var size = 0
-@export var score = 0
-@export var color_name = ""
-var is_vertical = true
-var start_position = Vector2(-1, -1)
--- Scripts/Singletons/signalbus.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-signal Rotate()
-signal Game_is_over(score: int)
-signal Username_sended(username: String)
