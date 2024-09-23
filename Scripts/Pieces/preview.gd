e4ac922d6e5d5a5d4b81484f5a44cc2afe99b3e9
Allan <allan.delatuillerie@gmail.com>
2024-09-23 14:11:36 +0200

    rm rotate methode

--- Scripts/Pieces/preview.gd
+++ Scripts/Pieces/preview.gd
@@ -1,14 +1,3 @@
 extends Control
 
 @onready var texture_rect: TextureRect = $TextureRect
-const ROTATION_DEGREE = -1.57079994678497
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	SignalBus.Rotate.connect(rotate_piece)
-
-func rotate_piece():
-	if texture_rect.rotation == 0:
-		texture_rect.rotation = -1.57079994678497
-	elif texture_rect.rotation != 0:
-		texture_rect.rotation = 0
