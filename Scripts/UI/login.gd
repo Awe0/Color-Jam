622e650fd1d7d4d53f16284345ac2bdc2bae3621
Allan <allan.delatuillerie@gmail.com>
2024-09-24 16:48:24 +0200

    OAuth OK

--- Scripts/UI/login.gd
+++ Scripts/UI/login.gd
@@ -1,45 +1,27 @@
 extends Control
 
-signal create_user(username: String, password: String)
-
-@onready var connection_username: TextEdit = $VBoxContainer/UsernameContainer/Label/MarginContainer/TextEdit
-@onready var connection_password : LineEdit = $VBoxContainer/PasswordContainer/Label/MarginContainer/LineEdit
 @onready var message_timer: Timer = $Messages/Timer
 @onready var failed: Label = $Messages/Failed
 @onready var success: Label = $Messages/Success
-@onready var no_account: Label = $"Messages/No account"
 
-func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+var _sign_in_retries := 5
 
-#func _on_signin_button_pressed() -> void:
-	#var query = SqlController.get_user_from_db(connection_username.text)
-	#if query != null:
-		#if connection_username.text and connection_password.text:
-			#if connection_password.text == query["password"]:
-				#UserSession.log_in_log_out()
-				#UserSession.save_session(query["id"], query["name"])
-				#get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
-			#else:
-				#print("NEIN")
-	#else:
-		#display_label_and_start_timer(no_account)
-#
-#func _on_submit_button_pressed() -> void:
-	#if connection_username.text and connection_password.text:
-		#var data = {
-			#"name": connection_username.text,
-			#"password": connection_password.text,
-		#}
-		#SqlController.database.insert_row("users",data)
-		#connection_username.text = ""
-		#connection_password.text = ""
-		#if failed.visible == true:
-			#failed.visible == false
-		#create_user.emit(connection_username.text,connection_password.text)
-		#display_label_and_start_timer(success)
-	#else:
-		#display_label_and_start_timer(failed)
+func _ready() -> void:
+	if not GodotPlayGameServices.android_plugin:
+		display_label_and_start_timer(failed)
+	
+	SignInClient.user_authenticated.connect(func(is_authenticated: bool):
+		if _sign_in_retries > 0 and not is_authenticated:
+			print("Trying to sign in!")
+			SignInClient.sign_in()
+			_sign_in_retries -= 1
+		
+		if _sign_in_retries == 0:
+			print("Sign in attemps expired!")
+		
+		if is_authenticated:
+			get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+	)
 
 func _on_timer_timeout(label: Label) -> void:
 	label.visible = false
