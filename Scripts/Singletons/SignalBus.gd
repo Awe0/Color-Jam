982d75dd87786876f2cab42d5353515e57f3cfe6
Allan <allan.delatuillerie@gmail.com>
2024-09-30 17:24:02 +0200

    Progress bar oK

--- Scripts/Singletons/SignalBus.gd
+++ Scripts/Singletons/SignalBus.gd
@@ -3,4 +3,5 @@ extends Node
 signal Restart_Game()
 signal Game_is_over(score: int)
 signal Username_sended(username: String)
-signal Score_changed(score: int, level: int)
+signal Score_changed(score: int)
+signal Level_up(level: int)
--- Scripts/UI/progress_bar.gd
+++ Scripts/UI/progress_bar.gd
@@ -4,16 +4,12 @@ extends ProgressBar
 
 func _ready() -> void:
 	SignalBus.Score_changed.connect(update_progress_bar)
+	SignalBus.Level_up.connect(change_max_value)
+	progress_bar.max_value = 100
+
+func change_max_value(level: int):
+	progress_bar.value = 0
+	progress_bar.max_value = 80
 
 func update_progress_bar(score: int):
-	progress_bar.value = score
-	#print("max value = " + str(progress_bar.max_value))
-	#print("actual value = " + str(progress_bar.value))
-	if progress_bar.value == progress_bar.max_value:
-		progress_bar.value = 0
-		#if level != 1:
-			#progress_bar.max_value = 80 
-			#level += 1
-			#level_label.text = "Level " + str(level)
-		#else:
-			#level += 1
+	progress_bar.value += score
