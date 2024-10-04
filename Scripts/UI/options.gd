8cff3d072eec9b1aa409fd4a7f64b372a8d92ca3
Allan <allan.delatuillerie@gmail.com>
2024-10-04 17:51:27 +0200

    refacto option but not finished

--- Scripts/UI/options.gd
+++ Scripts/UI/options.gd
@@ -1,7 +1,14 @@
 extends Control
 
 @onready var texture_button: TextureButton = $VBoxContainer/HBoxContainer2/TextureButton
-@onready var check_button: CheckButton = $VBoxContainer/RotationMode/CheckButton
+@onready var rotation_mode_button: CheckButton = $VBoxContainer/RotationMode/RotationModeButton
+@onready var dark_mode_button: CheckButton = $VBoxContainer/DarkMode/DarkModeButton
+
+const LIST_OPTIONS_STRING: Array = [
+	"rotation_mode",
+	"dark_mode"
+]
+
 
 func _ready() -> void:
 	set_config_saved()
@@ -9,15 +16,21 @@ func _ready() -> void:
 func _on_texture_button_pressed() -> void:
 	SignalBus.Restart_Game.emit()
 
-func _on_check_button_toggled(toggled_on: bool) -> void:
+func set_config_saved():
+	var config_data: Dictionary = {}
+	for option_string in LIST_OPTIONS_STRING:
+		if SaveSystem.load_config(option_string) == null:
+			for option in Config.lis
+			Config.rotation_mode = false
+		else:
+			config_data = SaveSystem.load_config("rotation_mode")
+			Config.rotation_mode = config_data["rotation_mode"]
+	rotation_mode_button.button_pressed = Config.rotation_mode
+
+func _on_rotation_mode_button_toggled(toggled_on: bool) -> void:
 	SaveSystem.save_config("game", "rotation_mode", toggled_on)
 	Config.rotation_mode = toggled_on
 
-func set_config_saved():
-	var config_data: Dictionary = {}
-	if SaveSystem.load_config("rotation_mode") == null:
-		Config.rotation_mode = false
-	else:
-		config_data = SaveSystem.load_config("rotation_mode")
-		Config.rotation_mode = config_data["rotation_mode"]
-	check_button.button_pressed = Config.rotation_mode
+func _on_dark_mode_button_toggled(toggled_on: bool) -> void:
+	SaveSystem.save_config("game", "dark_mode", toggled_on)
+	Config.dark_mode = toggled_on
