f140d53545cdc950fe2af9fafdc39d303312565a
Allan <allan.delatuillerie@gmail.com>
2024-10-15 12:33:12 +0200

    option saved and loaded works

--- Scripts/Singletons/Config.gd
+++ Scripts/Singletons/Config.gd
@@ -3,7 +3,7 @@ extends Node
 var rotation_mode: bool = false
 var dark_mode: bool = false
 
-var list_options: Array = [
-	rotation_mode,
-	dark_mode
-]
+var list_options: Dictionary = {
+	"rotation_mode" : rotation_mode,
+	"dark_mode" : dark_mode
+}
--- Scripts/Singletons/SaveSystem.gd
+++ Scripts/Singletons/SaveSystem.gd
@@ -4,8 +4,17 @@ const SAVE_CONFIG_DIRECTION: String = "user://save_config.cfg"
 
 func save_config(section: String, key: String, value: Variant):
 	var config_file = ConfigFile.new()
+	var err = config_file.load(SAVE_CONFIG_DIRECTION)
+	
+	if err != OK and err != ERR_FILE_NOT_FOUND:
+		print("Erreur lors du chargement du fichier de configuration")
+		return
+	
 	config_file.set_value(section, key, value)
-	config_file.save(SAVE_CONFIG_DIRECTION)
+	
+	err = config_file.save(SAVE_CONFIG_DIRECTION)
+	if err != OK:
+		print("Erreur lors de la sauvegarde du fichier de configuration")
 
 func load_config(key: String):
 	var config_data: Dictionary = {}
