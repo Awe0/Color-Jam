958774c2ad383a3bb0dc441f196b17fb9d2cb80c
Allan <allan.delatuillerie@gmail.com>
2024-10-16 15:14:00 +0200

    level_select in progress

--- Scripts/UI/level_select.gd
+++ Scripts/UI/level_select.gd
@@ -3,3 +3,6 @@ extends Control
 
 func _on_infinite_mode_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Game/Infinite_Mode.tscn")
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game/Level_1.tscn")
