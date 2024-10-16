6693769b5f2ef4be3c444bb95464a9a29f47a5e1
Allan <allan.delatuillerie@gmail.com>
2024-10-16 12:47:29 +0200

    Change button play path file

--- Scripts/UI/menu.gd
+++ Scripts/UI/menu.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	animated_sprite_2d.play("default")
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Game/Game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu/Level_Select.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
