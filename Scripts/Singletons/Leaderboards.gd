b8d2639572cd1796e3996d96631ae53880a1224b
Allan <allan.delatuillerie@gmail.com>
2024-10-21 13:32:11 +0200

    Add level 2 id

--- Scripts/Singletons/Leaderboards.gd
+++ Scripts/Singletons/Leaderboards.gd
@@ -2,5 +2,7 @@ extends Node
 
 const LEADERBOARDS_ID: Dictionary = {
 	"Infinite_mode" : "CgkIw9eFzccREAIQAg",
-	"Level_1" : "CgkIw9eFzccREAIQBQ"
+	"Level_1" : "CgkIw9eFzccREAIQBQ",
+	"Level_2" : "CgkIw9eFzccREAIQBg",
+	
 }
