ac0315195a371a50ade57720f30d29fa4b17a59e
Allan <allan.delatuillerie@gmail.com>
2024-10-22 11:51:20 +0200

    Add signals UI

--- Scripts/Singletons/SignalBus.gd
+++ Scripts/Singletons/SignalBus.gd
@@ -1,7 +1,14 @@
 extends Node
 
 signal Restart_Game()
-signal Game_is_over(score: int)
-signal Username_sended(username: String)
 signal Score_changed(score: int)
 signal Level_up(level: int)
+
+#Signaux pour l'UI IG
+signal Game_is_over()
+signal Rotating
+signal Rerolling
+signal Deleting
+signal Check_Grid
+
+signal Level_is_selected(level_name: String)
