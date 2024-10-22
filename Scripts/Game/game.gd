5a1bfa6c2003243b98e1e398d21e30d1367aa78a
Allan <allan.delatuillerie@gmail.com>
2024-10-22 13:21:53 +0200

    add fx

--- Scripts/Game/game.gd
+++ Scripts/Game/game.gd
@@ -68,6 +68,7 @@ func place_color(i: int, j: int):
 			cells[x][y].icon = load("res://Assets/Colors/"+ selected_color.color_name +".png")
 			cells[x][y].flat = true
 		#attempt += 1
+		AudioPlayer.play_random_bubble_FX()
 		preview.rotation = 0
 		update_piece_queue()
 		game_statement()
