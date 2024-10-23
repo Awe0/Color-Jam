8daebb9f82dd7419e168efe8e8683d690ba997ea
Allan <allan.delatuillerie@gmail.com>
2024-10-23 13:00:08 +0200

    Leaderboard created

--- Scripts/Singletons/Leaderboards.gd
+++ Scripts/Singletons/Leaderboards.gd
@@ -8,5 +8,14 @@ const LEADERBOARDS_ID: Dictionary = {
 	"level 4" : "CgkIw9eFzccREAIQCA",
 	"level 5" : "CgkIw9eFzccREAIQCQ",
 	"level 6" : "CgkIw9eFzccREAIQCg",
-	
+	"level 7" : "CgkIw9eFzccREAIQCw",
+	"level 8" : "CgkIw9eFzccREAIQDA",
+	"level 9" : "CgkIw9eFzccREAIQDQ",
+	"level 10" : "CgkIw9eFzccREAIQDg",
+	"level 11" : "CgkIw9eFzccREAIQDw",
+	"level 12" : "CgkIw9eFzccREAIQEA",
+	"level 13" : "CgkIw9eFzccREAIQEQ",
+	"level 14" : "CgkIw9eFzccREAIQEg",
+	"level 15" : "CgkIw9eFzccREAIQEw",
+	"level 16" : "CgkIw9eFzccREAIQFA"
 }
