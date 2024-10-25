81d892ff93c1127cbf7acc2864e85af4c18cb614
Allan <allan.delatuillerie@gmail.com>
2024-10-25 14:06:20 +0200

    Popup win works

--- Scripts/UI/game_over.gd
+++ Scripts/UI/game_over.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var background: ColorRect = $Background
 @onready var _3_stars: TextureRect = $"3Stars"
 @onready var _2_stars: TextureRect = $"2Stars"
 @onready var _1_star: TextureRect = $"1Star"
@@ -14,11 +15,17 @@ func check_attempt_for_stars_texture(level_name, attempt):
 	var silver_threshold = LevelStatement.level_attempt[level_name][1]
 	var bronze_threshold = LevelStatement.level_attempt[level_name][2]
 
+	print(attempt)
+
 	if attempt <= gold_threshold:
 		_3_stars.visible = true
+		background.visible = true
 	elif attempt <= silver_threshold:
 		_2_stars.visible = true
+		background.visible = true
 	elif attempt <= bronze_threshold:
 		_1_star.visible = true
+		background.visible = true
 	else:
 		_0_star.visible = true
+		background.visible = true
